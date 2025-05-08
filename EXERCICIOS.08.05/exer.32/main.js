0eb9f381645ffdc1941ac7cd47783a1d25c7c92e
Matheus Ventura de Oliveira <matheus.ventura.oliveira@escola.pr.gov.br>
2025-05-08 12:29:53 -0300

    Add files via upload

---
+++ EXERCICIOS.08.05/exer.32/main.js
@@ -0,0 +1,8 @@
+function adicionarTarefa(){
+    const input = document.getElementById('nova-tarefa');
+    const textoTarefa = input.value.trim();
+}
+
+if (textoTarefa === "") return;
+
+// paramos na linha 71 //
